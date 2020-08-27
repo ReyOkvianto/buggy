853e8e7c785d6f4cdf08671441b40ba65dbd23bd
Rey (Reyandra) Okvianto <55027338+ReyOkvianto@users.noreply.github.com>
2020-08-27 14:03:20 -0400

    Add files via upload

--- buggy.cpp
+++ buggy.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+//add the std vector library
 #include <vector>
 
+//use the std namespace to avoid redundancy of std::
 using namespace std;
  //Blank A
 class node {
@@ -16,14 +18,25 @@ void create_LL(vector<node*>& mylist, int node_num){
     //create a set of nodes
     for (int i = 0; i < node_num; i++) {
         //Blank C
+        //solved the first error
+        //allocate memory on the heap for each 
         mylist[i] = new node;
+        //changed the . method to the ->
+        //since each element in mylist is a object pointer and not an object
         mylist[i]->val = i;
         mylist[i]->next = NULL;
     }
 
     //create a linked list
-    //adjust the bounds
+    //second error
+    //fixed the bounds of iteration because once the loop gets to the 
+    //last index, it tries to assign nothing to the previous pointer
+    //NOTE: addressSanitizer pointed out that I received an out of bounds error
+    //got rid of lots of memory leaks "fa fa fa...""
     for (int i = 0; i < node_num - 1; i++) {
+        //adjust the bounds to not go out
+        //changed the . method to the ->
+        //since each element in mylist is a object pointer and not an object
         mylist[i]->next = mylist[i+1];
     }
 }
@@ -31,6 +44,8 @@ void create_LL(vector<node*>& mylist, int node_num){
 int sum_LL(node* ptr) {
     int ret = 0;
     while(ptr) {
+        //changed the . method to the ->
+        //since each element in mylist is a object pointer and not an object
         ret += ptr->val;
         ptr = ptr->next;
     }
@@ -41,16 +56,14 @@ int main(int argc, char ** argv){
     const int NODE_NUM = 3;
     vector<node*> mylist;
 
-    // //initialize mylist
-    // for(int i = 0; i < NODE_NUM; i++){
-    //     mylist.push_back(new node);
-    // }
-
     create_LL(mylist, NODE_NUM);
     int ret = sum_LL(mylist[0]); 
     cout << "The sum of nodes in LL is " << ret << endl;
 
     //Step4: delete nodes
+    //last error fixed
+    //for loop acts a destructor to deallocate the memory after we manipulate it
+    //got rid of the rest of my errors
     for(int i = 0; i < NODE_NUM; i++) {
         delete mylist[i];
     }
